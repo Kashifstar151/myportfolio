df5303318f78ed3ab3f21ce9aab69d3a9cd327d9
kashif shoeb <kashif.shoeb@mogiio.com>
2022-11-05 18:53:59 +0530

    add all

---
+++ src/components/Home/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./home.css"
+
+const Home = () => {
+  return (
+    <div className='home-div'>
+        <div className='content-div'>centered div
+        <div >
+            <h1>hi my name is kashif</h1>
+        </div>
+        </div>
+        
+    </div>
+  )
+}
+
+export default Home
